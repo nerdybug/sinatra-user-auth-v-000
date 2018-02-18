f09009f6f177332009b274dc50a4ad30a089bc0b
Meredith Reece <mardebugger@gmail.com>
2018-02-18 02:43:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
 
   get '/users/home' do
     @user = User.find_by(id: session[:id])
-    binding.pry
+    # binding.pry
     erb :'/users/home'
     status 200
   end
